69a4c5e8cba443ea460da054de514fc40336c61b
officialrajdeepsingh <radewala.2@gmail.com>
2022-06-15 17:35:31 +0530

    add google ads

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9978927393009784"
+     crossorigin="anonymous"></script>
+      </body>
+    </Html>
+  )
+}
